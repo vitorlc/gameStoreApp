5dbd77f389f02368d7c0fb537e32f8e9b834b586
vitorlc <vitor_cirqueira1@hotmail.com>
2020-12-01 20:38:36 -0300

    Add home page

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './pages/Home'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Game Store App' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#343434',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
